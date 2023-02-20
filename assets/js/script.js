5388143312a44d99574baf54f38793d56092d8a4
JakCrane <jackx.crane@hotmail.com>
2023-02-20 20:32:56 +0000

    started score features

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ class Socket {
         this.age = Math.random();
         this.typeMatch = 0;
         this.canMerge = false
+        this.root = false
     }
     get leftType() {
         if (parseInt(this.positionx) == 0) {return null}
@@ -96,31 +97,37 @@ class QueuePosition {
 }
 var socketArr = [];
 var queueArr = [];
+var scoreTotal = 0;
+var scorePopup = 0;
 document.addEventListener("DOMContentLoaded", function() {
     startGame(); 
     let socketsEl = document.getElementsByClassName("socket")
     for (let socketEl of socketsEl) {
         socketEl.addEventListener("click", function() {
             let socket = socketArr.filter(socket => socket.positionx == this.getAttribute("id").split("-")[0]).filter(socket => socket.positiony == this.getAttribute("id").split("-")[1])[0]
+            socketArr.filter(socket => socket.root == true)[0].root = false
             if (socket.type != "empty") {return}
             for (let socketAger of socketArr) {socketAger.age++}
             socket.type = queueArr[0].getAndProgressQueue()
-            socket.age = 0
+            socket.age = 0;
+            socket.root = true;
             refreshQueue()
-            refreshSockets()
             canMerge()
             if (checkEnd()) {endGame()}
         });
     }
-    //let endGameButton = document.getElementById("endGame")
-    //endGameButton.addEventListener("click", endGame())
+    document.getElementById("endGame").addEventListener("click", endGame())
+    document.getElementById("refresh").addEventListener("click", refreshSockets())
 });
 startGame = () => {
     initialiseSockets();
+    socketArr[0].root = true
     initialiseQueue();
     refreshQueue();
     canMerge();
     refreshSockets();
+    scoreTotal = 0;
+    document.getElementById("score").innerText = `Score: ${scoreTotal}`;
 }
 initialiseSockets = () => {
     let sockets = document.getElementsByClassName("socket")
@@ -146,26 +153,26 @@ initialiseQueue = () => {for (let i = 0; i<3; i++) {queueArr[i] = new QueuePosit
  * refreshes sockets
  */
 refreshSockets = () => {
-    for (let socket of socketArr.filter(socket => socket.type != "empty")) {
-        if (socket.canMerge) {document.getElementById(`${socket.positionx}-${socket.positiony}`).setAttribute("class", `${socket.type} merge socket`)}
+    
+    for (let socket of socketArr) {
+        if (socket.root) {document.getElementById(`${socket.positionx}-${socket.positiony}`).setAttribute("class", `${socket.type} socket root`)} 
         else {document.getElementById(`${socket.positionx}-${socket.positiony}`).setAttribute("class", `${socket.type} socket`)}
     }
-    for (let socket of socketArr.filter(socket => socket.type == "empty")) {document.getElementById(`${socket.positionx}-${socket.positiony}`).setAttribute("class", "empty socket")}
 }
 refreshQueue = () => {
     for (let queue of queueArr) {
         document.getElementById(`${queue.number}`).setAttribute("class", `${queue.type}`)
     }
 }
-
-
 /**
  * can merge takes in sockets and checks if typeMatch > 1, if so it sets canMerge to true calls propogation and refreshes
  */
 canMerge = () => {
-    console.log(socketArr.filter(socket => socket.type != "empty"))
+    console.log(socketArr.filter(socket => socket.type != "empty")) //sometimes merging emptys which doesnt affect game play but is cringe
     for (let socket of socketArr.filter(socket => socket.type != "empty")) {socket.canMergeMethod()}
     console.log(socketArr.filter(socket => socket.type != "empty"))
+    merge()
+    refreshSockets()
 }
 /**
  * If a neighboring socket can merge it passes on the value
@@ -178,13 +185,7 @@ mergeAttributePropogation = () => {
         if (socket.belowMergeReadiness) {socket.canMerge = true; mergeAttributePropogation(socketArr)}
     }
 }
-merge = () => {
-    console.log("sleeping")
-    sleep(1000)
-    console.log("refreshing")
-    refreshSockets()
-    console.log("sleeping")
-    sleep(1000)
+merge = () => { //need to get it to display the socket being placed, then merge then display then repeat if needed
     let toMerge = []
     for (let socket of socketArr.filter(socket => socket.canMerge == true)) {toMerge.push(socket)}
     if (toMerge.length == 0) {console.log("no merges");return}
@@ -200,26 +201,25 @@ combine = ([root, ...rest]) => {
     console.log("combine called")
     console.log(root, rest)
     switch (root.type) {
-        case "grass": root.type = "bush"; break;
-        case "bush": root.type = "tree"; break;
-        case "tree": root.type = "hut"; break;
-        case "hut": root.type = "house"; break;
+        case "grass": root.type = "bush"; scorePopup = 100; break;
+        case "bush": root.type = "tree"; scorePopup = 400; break;
+        case "tree": root.type = "hut"; scorePopup = 1600; break;
+        case "hut": root.type = "house"; scorePopup = 6400; break;
     }
+    scoreTotal += scorePopup
+    document.getElementById("score-popup").innerText = `${scorePopup}`
+    document.getElementById("score").innerText = `Score: ${scoreTotal}`
     root.canMerge = false
     for (let socket of rest) {
         socket.type = "empty"
         socket.canMerge = false
     }
 }
-checkEnd = () => {
-    for (let socket of socketArr) {
-        if (socket.value == "empty") {return false}
-        return true
-    }
+checkEnd = () => {for (let socket of socketArr.filter(socket => socket.value == "empty")) {return true}}
+endGame = () => {
+    alert("you lose")
+    //document.getElementById('score-card').setAttribute("id") = "unhidden-score-card"
 }
-//endGame = () => {
-//    document.getElementById('score-card').setAttribute("id") = "unhidden-score-card"
-//}
 
 
 //copied from online
@@ -232,3 +232,6 @@ function sleep(milliseconds) {
   }
   
 
+ask richey to sort refresh feature to see 3 then combo 
+ask richey to sort position absolute
+ask richey to sort event listeners firing when assigned
